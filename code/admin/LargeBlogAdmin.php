8572870af89c3025a64844210f19d337e9748db3
Tim Klein <tim@dodat.de>
2012-11-14 20:00:09 -0800

    removed function to load versionedgridfield detail form, as this is now handled by versionedmodeladmin. This makes this module somewhat obsolete. Will think about whether it makes sense to keep this going... --this line, and those below, will be ignored--
    M    code/admin/LargeBlogAdmin.php

--- code/admin/LargeBlogAdmin.php
+++ code/admin/LargeBlogAdmin.php
@@ -13,25 +13,5 @@ class LargeBlogAdmin extends ModelAdmin {
 	static $url_segment = "blog_admin";
 	
 	static $menu_title = "Blog";
-	
-	function getEditForm($id = null, $fields = null) {
-		$form = parent::getEditForm();
-
-		$fieldList = $form->Fields();
-
-		foreach($fieldList as $field) {
-			if($field instanceof GridField) {
-				$class = $field->getList()->dataClass();
-				if(Object::has_extension($class, "Versioned")) {
-					$config = $field->getConfig();
-					$config->removeComponentsByType('GridFieldDeleteAction')
-						->removeComponentsByType('GridFieldDetailForm')
-						->addComponents(new VersionedGridFieldDetailForm());
-					$field->setConfig($config);
-				}
-			}
-		}
-		return $form;
-	}
 
 }
